3d7ceac9520aed8df4c12af583cf49d3db5c6844
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-23 22:43:08 +1000

    Create ArithmeticProgression.py

---
+++ ArithmeticProgression.py
@@ -0,0 +1,8 @@
+one_number = int(input())
+two_number = int(input())
+three_number = int(input())
+
+if three_number - two_number == two_number - one_number:
+    print('YES')
+else:
+    print('NO')

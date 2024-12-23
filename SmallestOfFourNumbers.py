999010534fb76b366b6ed071237c00400dfe45db
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-12-23 22:53:52 +1000

    Create SmallestOfFourNumbers.py

---
+++ SmallestOfFourNumbers.py
@@ -0,0 +1,13 @@
+numbers = []
+
+first_user_number = int(input())
+second_user_number = int(input())
+third_user_number = int(input())
+fourth_user_number = int(input())
+
+numbers.append(first_user_number)
+numbers.append(second_user_number)
+numbers.append(third_user_number)
+numbers.append(fourth_user_number)
+
+print(min(numbers))
